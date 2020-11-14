aa6f57c31ccdb7f30248e80d3104c340b64a3095
ujjwal <ujjwal2022.cs19@nsut.ac.in>
2020-11-15 04:26:03 +0530

    added

---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+import * as firebase from 'firebase';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyCj0UwlG8w_GYUFA3JjsCf-nmck9Ly7pNM',
+  authDomain: 'hackathon-9f324.firebaseapp.com',
+  databaseURL: 'https://hackathon-9f324.firebaseio.com',
+  projectId: 'hackathon-9f324',
+  storageBucket: 'hackathon-9f324.appspot.com',
+  messagingSenderId: '777529751920',
+  appId: '1:777529751920:web:24ba576cd5b8b25b7e37b5'
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.firestore();
+
+const Auth = firebase.auth();
+
+export { database, Auth };
