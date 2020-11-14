6356afd1120fc3c956fe24649a7f8a2a8ed3f598
ujjwal <ujjwal2022.cs19@nsut.ac.in>
2020-11-15 04:58:18 +0530

    ok

--- src/MainUI.js
+++ src/MainUI.js
@@ -24,7 +24,7 @@ export default function MainUI() {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar position="fixed">
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
