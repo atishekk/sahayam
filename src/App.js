1746f1be604eb2bb107627331733bc278ade8c02
Tarush Bajaj <54022264+Tarushfx@users.noreply.github.com>
2020-11-15 18:52:17 +0530

    created event cards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import MainUI from './MainUI';
-import CreateUser from './Components/CreateUser';
-import NGODashBoard from './Components/NGOdashboard';
-import SignIn from './Components/SignIn';
-import UserDashBoard from './Components/Userdashboard';
-import AboutUs from "./Components/AboutUS"
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import MainUI from "./MainUI";
+import CreateUser from "./Components/CreateUser";
+import NGODashBoard from "./Components/NGOdashboard";
+import SignIn from "./Components/SignIn";
+import UserDashBoard from "./Components/Userdashboard";
+import AboutUs from "./Components/AboutUS";
+import GetPost from "./Components/EventPost";
+import EventFeed from "./Components/EventFeed";
 
 const App = () => {
-    return (
-        <React.Fragment>
-            <MainUI/>
-            <Switch>
-                <Route exact path="/" component={AboutUs} />
-                <Route path="/createuser" component={CreateUser}/>
-                <Route path="/signin" component={SignIn} />
-                <Route path="/dashboardUser" component={UserDashBoard} />
-                <Route path="/dashboardNGO" component={NGODashBoard} />
-            </Switch>
-        </React.Fragment>
-    )
-}
+  return (
+    <React.Fragment>
+      <MainUI />
+      <Switch>
+        <Route exact path="/">
+          <AboutUs />
+          <EventFeed />
+        </Route>
+        <Route path="/createuser" component={CreateUser} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/dashboardUser" component={UserDashBoard} />
+        <Route path="/dashboardNGO" component={NGODashBoard} />
+      </Switch>
+    </React.Fragment>
+  );
+};
 
 export default App;
--- src/Components/AboutUS.js
+++ src/Components/AboutUS.js
@@ -1,25 +1,25 @@
 import React from "react";
 import Carousel from "react-material-ui-carousel";
-import {Paper} from "@material-ui/core";
+import { Paper } from "@material-ui/core";
 
-const imageSource = []
+const imageSource = [];
 
 const AboutUS = () => {
-    return (
-        <Carousel>
-            {
-                imageSource.map( x => <Image source={x} key={imageSource.indexOf(x)}/> )
-            }
-        </Carousel>
-    )
-}
+  return (
+    <Carousel>
+      {imageSource.map((x) => (
+        <Image source={x} key={imageSource.indexOf(x)} />
+      ))}
+    </Carousel>
+  );
+};
 
 const Image = (props) => {
-    return (
-        <Paper variant="outlined" elevation={5}>
-            <img src={props.source}/>
-        </Paper>
-    )
-}
+  return (
+    <Paper variant="outlined" elevation={5}>
+      <img src={props.source} />
+    </Paper>
+  );
+};
 
-export default AboutUS
+export default AboutUS;
