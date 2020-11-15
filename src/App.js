4e8b424ee654da0cc56840c42dbdaff63c31edfb
Atishek Kumar <atishekkumar19@gmail.com>
2020-11-15 17:22:18 +0530

    0

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import CreateUser from './Components/CreateUser';
 import NGODashBoard from './Components/NGOdashboard';
 import SignIn from './Components/SignIn';
 import UserDashBoard from './Components/Userdashboard';
+import AboutUs from "./Components/AboutUS"
 
 const App = () => {
     return (
         <React.Fragment>
             <MainUI/>
             <Switch>
+                <Route exact path="/" component={AboutUs} />
                 <Route path="/createuser" component={CreateUser}/>
                 <Route path="/signin" component={SignIn} />
                 <Route path="/dashboardUser" component={UserDashBoard} />
